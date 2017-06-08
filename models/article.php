6043104728ca6d88287ac4b549f3d00c628cb04d
Jerous <tlu37317@gmail.com>
2017-06-08 14:15:21 +0800

    model從sql語法改用joomla model提供的update與insert的方法

--- models/article.php
+++ models/article.php
@@ -28,37 +28,46 @@ class BlogModelArticle extends JModelLegacy
 
         $id = $data['id'];
 
-        // 把 $data 內的每個元素用單引號包起來，並且跳脫不合法字元
-        foreach ($data as &$value)
-        {
-            $value = $db->quote($value);
-        }
+        // // 把 $data 內的每個元素用單引號包起來，並且跳脫不合法字元
+        // foreach ($data as &$value)
+        // {
+        //     $value = $db->quote($value);
+        // }
+
+        //改用 insertObject() 與 updateObject() 來建立或更新資料，這兩個方法只吃物件
+        $data = (object) $data;
 
         // 有 id 時用 update
         if ($id)
         {
-            $sql = "UPDATE `wizhb_blog_articles` SET "
-                . "`title` = " . $data['title']
-                . ", `alias` = " . $data['alias']
-                . ", `created` = " . $data['created']
-                . ", `introtext` = " . $data['introtext']
-                . ", `fulltext` = " . $data['fulltext']
-                . " WHERE `id` = " . $data['id'];
+            // $sql = "UPDATE `wizhb_blog_articles` SET "
+            //     . "`title` = " . $data['title']
+            //     . ", `alias` = " . $data['alias']
+            //     . ", `created` = " . $data['created']
+            //     . ", `introtext` = " . $data['introtext']
+            //     . ", `fulltext` = " . $data['fulltext']
+            //     . " WHERE `id` = " . $data['id'];
+
+            // updateObject() 的第三個參數是必須的，我們要告訴DB物件以 id 為判斷標準來更新
+            $db->updateObject('wizhb_blog_articles', $data, 'id');
         }
 
         // 沒有 id 時用 insert
         else
         {
-            // fulltext 是 SQL 保留字，我們要用反引號 (`) 把 fulltext 包起來才能當做欄位名稱
-            // 送進去的 data 用 array_map() 來替每個值做引號包裹與跳脫
-            $sql = "INSERT INTO `wizhb_blog_articles`"
-                . " (`id`, `title`, `alias`, `created`, `introtext`, `fulltext`) "
-                . " VALUES (" . implode(', ', $data) . ")";
+            // // fulltext 是 SQL 保留字，我們要用反引號 (`) 把 fulltext 包起來才能當做欄位名稱
+            // // 送進去的 data 用 array_map() 來替每個值做引號包裹與跳脫
+            // $sql = "INSERT INTO `wizhb_blog_articles`"
+            //     . " (`id`, `title`, `alias`, `created`, `introtext`, `fulltext`) "
+            //     . " VALUES (" . implode(', ', $data) . ")";
+
+            $db->insertObject('wizhb_blog_articles', $data);
         }
 
-        $db->setQuery($sql);
+        // $db->setQuery($sql);
 
-        return $db->execute();
+        // return $db->execute();
+        return true;
     }
 
     public function delete($id)
